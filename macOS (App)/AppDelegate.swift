2d5e3b1138e884fafd3de91c23453f6835336a02
Jordan Christensen <jordan.c4922@gmail.com>
2026-03-11 10:30:03 -0600

    Initial Commit

---
+++ macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Jordan Christensen on 3/11/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
