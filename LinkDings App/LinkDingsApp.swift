6db1530fce05c93fe205287e319fd775438aa920
Jordan C <jordan.c4922@gmail.com>
2026-03-12 15:08:09 -0600

    Fix issue with non-local http connections failing

--- LinkDings App/LinkDingsApp.swift
+++ LinkDings App/LinkDingsApp.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 import SwiftData
 
-// Fix App Transport Security policy to allow non-secure connections from non-local networks (try 100.100.100.100:1000 to see the issue)
 // When searching, do an on string change debounced search instead of waiting on enter
 // When opening a bookmark's edit page, it should fetch the latest data from the server
 // Clicking a bookmark should open it in the default web brower, long pressing should show a context menu
